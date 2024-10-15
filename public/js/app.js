a827c3859d9a748399316736f52f732e6fcc043c
Anastasios Simitzis <tasossimitzis1@gmail.com>
2024-10-15 14:18:23 +0300

    order list impl

--- public/js/app.js
+++ public/js/app.js
@@ -6,3 +6,13 @@ function toggleText(id) {
         element.classList.add("show");
     }
 }
+
+function toggleContent(id){
+    var content = document.getElementById(id);
+    if(content.style.display==="none"){
+        content.style.display = "block";
+    }
+    else{
+        content.style.display = "none";
+    }
+}
